191d3f7e894a679a67c2dd30403f63c6e2939bf5
podayousef123-hue <podayousef123@gmail.com>
2025-08-29 08:43:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-./dnstt-server -udp :443 khartoshtop.qzz.io server.key
+
+# Build dnstt-server
+git clone https://www.bamsoftware.com/git/dnstt.git
+cd dnstt/dnstt-server
+make
+
+# ارجع للمجلد الأصلي
+cd ../../
+
+# شغّل السيرفر
+./dnstt/dnstt-server/dnstt-server -udp :443 khartoshtop.qzz.io server.key
